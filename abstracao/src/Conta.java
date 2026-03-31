0fe17e417d66f2273967a96cb9722666a670a0a3
Daniel Gomes <danielvava669@gmail.com>
2026-03-31 11:10:58 -0300

    Exercicio abstração

---
+++ abstracao/src/Conta.java
@@ -0,0 +1,76 @@
+public abstract class Conta {
+    private float numero;
+    private String agencia;
+    private String titular;
+    private double saldo;
+
+    public Conta() {
+
+    }
+
+    public void depositar(double valor) {
+        saldo = saldo + valor;
+        IO.println("deposito feito com sucesso " + " seu saldo é " + saldo);
+
+    }
+
+    public void sacar(double valor) {
+        saldo = saldo - valor;
+        IO.println("saque feito com sucesso " + " seu saldo é " + exibirSaldo());
+
+    }
+
+    public double exibirSaldo() {
+        return saldo;
+
+    }
+
+    public abstract void calcularTarifaMensal();
+
+
+    public Conta(float numero, String agencia, String titular, float saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public float getNumero() {
+        return numero;
+    }
+
+    public void setNumero(float numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
+
+
+
+
+
+
+
